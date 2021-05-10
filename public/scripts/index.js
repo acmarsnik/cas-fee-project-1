73eee669e17d81c4fb7b260d1c786e69e029bc50
albertmarsnik <albertmarsnik@gmail.com>
2021-05-10 23:10:34 +0200

    add basic navigation between index and create-edit-note with accessing hard coded note-ids on edit buttons

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,20 @@
 console.log('index.js');
+
+function navigateToEdit($event) {
+    console.log({ $event });
+    const attributes = $event?.target?.attributes;
+    const noteId = attributes ? attributes['note-id'].value : null;
+    console.log({ noteId });
+    alert(noteId);
+    window.location.assign('/create-edit-note.html');
+}
+
+document
+    .querySelector('.edit-button button[note-id="1"]')
+    .addEventListener('click', navigateToEdit);
+document
+    .querySelector('.edit-button button[note-id="2"]')
+    .addEventListener('click', navigateToEdit);
+document
+    .querySelector('.edit-button button[note-id="3"]')
+    .addEventListener('click', navigateToEdit);
