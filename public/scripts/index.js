7aaffdca515cde0e4307b284075433f4f16eda84
albertmarsnik <albertmarsnik@gmail.com>
2021-05-20 12:23:04 +0200

    replaced hard coded notes with handlebars each loop and hard coded javascript object for the data

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,20 +1,71 @@
-console.log('index.js');
-
 function navigateToEdit($event) {
-    console.log({ $event });
     const attributes = $event?.target?.attributes;
     const noteId = attributes ? attributes['note-id'].value : null;
-    console.log({ noteId });
     alert(noteId);
     window.location.assign('/create-edit-note.html');
 }
 
-document
-    .querySelector('.edit-button button[note-id="1"]')
-    .addEventListener('click', navigateToEdit);
-document
-    .querySelector('.edit-button button[note-id="2"]')
-    .addEventListener('click', navigateToEdit);
-document
-    .querySelector('.edit-button button[note-id="3"]')
-    .addEventListener('click', navigateToEdit);
+const notesContainer = document.getElementById('notes-template').innerHTML;
+document.getElementById('notes-template').remove();
+const notesContainerTemplate = Handlebars.compile(notesContainer);
+
+const notesContext = {
+    notes: [
+        {
+            id: 1,
+            finishByDate: 'Nächsten Mittwoch',
+            title: 'CAS FEE Selbststudium / Projekt Aufgabe erledigen',
+            importance: 2,
+            isFinished: true,
+            finishDate: 'Today',
+            description:
+                'HTML für die note App erstellen. <br /> CSS erstellen für die note App [...]',
+            hasArrowDown: true,
+        },
+        {
+            id: 2,
+            finishByDate: 'Heute',
+            title: 'Einkaufen',
+            importance: 1,
+            isFinished: false,
+            finishDate: null,
+            description: 'Butler <br /> Eier [...]',
+            hasArrowDown: true,
+        },
+        {
+            id: 3,
+            finishByDate: 'Irgendwann',
+            title: 'Mami anrufen',
+            importance: 0,
+            isFinished: false,
+            finishDate: null,
+            description: '888 888 88 88',
+            hasArrowDown: false,
+        },
+    ],
+};
+
+const notesContainerHtml = notesContainerTemplate(notesContext);
+document.getElementById('index-page-container').innerHTML += notesContainerHtml;
+
+notesContext.notes.forEach((note) => {
+    document
+        .querySelector(`.edit-button button[note-id="${note.id}"]`)
+        .addEventListener('click', navigateToEdit);
+});
+
+const bolts = [1, 2, 3, 4, 5];
+const importanceElements = document.querySelectorAll('.importance');
+let importanceElementIndex = 0;
+
+importanceElements.forEach((importanceElement) => {
+    let importance = notesContext.notes[importanceElementIndex].importance;
+    bolts.forEach((boltNumber) => {
+        let hiddenString = getHiddenString(importance, boltNumber);
+        importanceElement.innerHTML += `<div class="bolt-container">
+                                            <div class="bolt black${hiddenString}"></div>
+                                        </div>`;
+    });
+
+    importanceElementIndex++;
+});
