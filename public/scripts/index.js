fbee76b99ee0307c986fb1e820640dbb6988b059
albertmarsnik <albertmarsnik@gmail.com>
2021-05-20 17:33:53 +0200

    functionalized index.js,, moved date related stuff out to notes service

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,7 @@
+import * as NotesService from './notes-service.js';
+
+const notesTemplate = document.getElementById('notes-template').innerHTML;
+
 function navigateToEdit($event) {
     const attributes = $event?.target?.attributes;
     let noteId = null;
@@ -9,67 +13,105 @@ function navigateToEdit($event) {
     );
 }
 
-const notesContainer = document.getElementById('notes-template').innerHTML;
-document.getElementById('notes-template').remove();
-const notesContainerTemplate = Handlebars.compile(notesContainer);
+function getNotesContext() {
+    return {
+        notes: NotesService.getNotes(),
+    };
+}
 
-const today = new Date();
-const yesterday = new Date();
-yesterday.setDate(today.getDate() - 1);
-const twoDaysAgo = new Date();
-twoDaysAgo.setDate(today.getDate() - 2);
+function addNotesEventListeners(notesContext) {
+    notesContext.notes.forEach((note) => {
+        document
+            .querySelector(`.edit-button button[note-id="${note.id}"]`)
+            .addEventListener('click', navigateToEdit);
+    });
 
-const notesContext = {
-    notes: [
-        {
-            id: 1,
-            finishByDate: 'Nächsten Mittwoch',
-            title: 'CAS FEE Selbststudium / Projekt Aufgabe erledigen',
-            importance: 2,
-            isFinished: true,
-            finishedDate: today,
-            finishedDateDisplay: today.toDateString(),
-            shortDescription:
-                'HTML für die note App erstellen. <br /> CSS erstellen für die note App [...]',
-            fullDescription:
-                'HTML für die note App erstellen. <br /> CSS erstellen für die note App <br /> and other stuff too.',
-            hasExpand: true,
-        },
-        {
-            id: 2,
-            finishByDate: 'Heute',
-            title: 'Einkaufen',
-            importance: 1,
-            isFinished: false,
-            finishedDate: yesterday,
-            finishedDateDisplay: yesterday.toDateString(),
-            shortDescription: 'Butler <br /> Eier [...]',
-            fullDescription: 'Butler <br /> Eier <br /> Brot',
-            hasExpand: true,
-        },
-        {
-            id: 3,
-            finishByDate: 'Irgendwann',
-            title: 'Mami anrufen',
-            importance: 0,
-            isFinished: false,
-            finishedDate: twoDaysAgo.toDateString(),
-            finishedDateDisplay: twoDaysAgo.toDateString(),
-            shortDescription: null,
-            fullDescription: '888 888 88 88',
-            hasExpand: false,
-        },
-    ],
-};
+    const arrowElements = document.querySelectorAll(`[id^="arrow-"]`);
 
-const notesContainerHtml = notesContainerTemplate(notesContext);
-document.getElementById('index-page-container').innerHTML += notesContainerHtml;
+    arrowElements.forEach((arrowElement) => {
+        arrowElement.addEventListener('click', toggleDescriptionsAndArrows);
+    });
 
-notesContext.notes.forEach((note) => {
     document
-        .querySelector(`.edit-button button[note-id="${note.id}"]`)
+        .getElementById('create-note')
         .addEventListener('click', navigateToEdit);
-});
+}
+
+function addImportanceElements(notesContext) {
+    const bolts = [1, 2, 3, 4, 5];
+    const importanceElements = document.querySelectorAll('.importance');
+
+    function getHiddenString(importance, boltNumber) {
+        return importance && importance >= boltNumber ? '' : ' hidden';
+    }
+
+    let importanceElementIndex = 0;
+
+    importanceElements.forEach((importanceElement) => {
+        let importance = notesContext.notes[importanceElementIndex].importance;
+        bolts.forEach((boltNumber) => {
+            let hiddenString = getHiddenString(importance, boltNumber);
+            importanceElement.innerHTML += `<div class="bolt-container">
+                                            <div class="bolt black${hiddenString}"></div>
+                                        </div>`;
+        });
+
+        importanceElementIndex++;
+    });
+}
+
+function removeAllNotesTemplateElements() {
+    const notesTemplateElementSelectors = [
+        '.finish-by-date',
+        '.title-padding-left',
+        '.title',
+        '.space-between-title-and-importance',
+        '.importance',
+        '.importance-padding-right',
+        '.finished',
+        '.description-padding-left',
+        '.description',
+        '.space-between-description-and-arrow',
+        '.arrow-container',
+        '.arrow-padding-right',
+        '.move-edit-down',
+        '.edit-button',
+    ];
+    const notesTemplateElements = [];
+    notesTemplateElementSelectors.forEach((notesTemplateElementSelector) => {
+        notesTemplateElements.push(
+            document.querySelectorAll(notesTemplateElementSelector)
+        );
+    });
+    notesTemplateElements.forEach((notesTemplateElement) => {
+        notesTemplateElement.forEach((item) => {
+            item.remove();
+        });
+    });
+}
+
+function removeTemplate() {
+    const notesTemplateElement = document.getElementById('notes-template');
+    if (document.body.contains(notesTemplateElement)) {
+        document.getElementById('notes-template').remove();
+    } else {
+        removeAllNotesTemplateElements();
+    }
+}
+
+function updateNotes(notesTemplate) {
+    removeTemplate();
+    const notesContainerTemplate = Handlebars.compile(notesTemplate);
+
+    const notesContext = getNotesContext();
+
+    const notesContainerHtml = notesContainerTemplate(getNotesContext());
+    document.getElementById('index-page-container').innerHTML +=
+        notesContainerHtml;
+
+    addNotesEventListeners(notesContext);
+    addImportanceElements(notesContext);
+}
 
 function toggleDescriptionsAndArrows($event) {
     const attributes = $event?.target?.attributes;
@@ -82,33 +124,4 @@ function toggleDescriptionsAndArrows($event) {
     });
 }
 
-const arrowElements = document.querySelectorAll(`[id^="arrow-"]`);
-
-arrowElements.forEach((arrowElement) => {
-    arrowElement.addEventListener('click', toggleDescriptionsAndArrows);
-});
-
-const bolts = [1, 2, 3, 4, 5];
-const importanceElements = document.querySelectorAll('.importance');
-
-function getHiddenString(importance, boltNumber) {
-    return importance && importance >= boltNumber ? '' : ' hidden';
-}
-
-let importanceElementIndex = 0;
-
-importanceElements.forEach((importanceElement) => {
-    let importance = notesContext.notes[importanceElementIndex].importance;
-    bolts.forEach((boltNumber) => {
-        let hiddenString = getHiddenString(importance, boltNumber);
-        importanceElement.innerHTML += `<div class="bolt-container">
-                                            <div class="bolt black${hiddenString}"></div>
-                                        </div>`;
-    });
-
-    importanceElementIndex++;
-});
-
-document
-    .getElementById('create-note')
-    .addEventListener('click', navigateToEdit);
+updateNotes(notesTemplate);
