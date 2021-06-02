cbebb9547331b8fc211a3f0ef636d2219614637d
albertmarsnik <albertmarsnik@gmail.com>
2021-06-02 16:52:04 +0200

    fix npm run start with prod mode

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // server.js
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
+import express from 'express';
+import favicon from 'express-favicon';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/public/favicon.png'));
+app.use(favicon(__dirname + '\\favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/public/index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 app.listen(port);
