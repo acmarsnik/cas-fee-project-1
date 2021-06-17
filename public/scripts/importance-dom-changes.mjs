dba43e6c74c25485216d6f44315fd8393f27fd60
albertmarsnik <albertmarsnik@gmail.com>
2021-06-17 17:49:22 +0200

    fix importance eslint errors

---
+++ public/scripts/importance-dom-changes.mjs
@@ -0,0 +1,88 @@
+/* global $event */
+
+export default class ImportanceDomChanges {
+    static adjustImportanceColorsOnClick($event) {
+        const newImportanceValue = ImportanceDomChanges.getNewImportanceValue($event);
+        const { createEditBoltsToBeBlackElements, createEditBoltsToBeGrayElements } =
+            ImportanceDomChanges.getCreateEditBoltsToBeElements(newImportanceValue);
+        ImportanceDomChanges.adjustElementColors(createEditBoltsToBeBlackElements, 'black', 'gray');
+        ImportanceDomChanges.adjustElementColors(createEditBoltsToBeGrayElements, 'gray', 'black');
+    }
+
+    static getNewImportanceValue($event) {
+        const boltContainerElement = ImportanceDomChanges.getBoltContainerElement($event);
+        let newImportanceValue = parseInt(boltContainerElement.dataset?.boltNumber, 10);
+        const previousImportanceValue = ImportanceDomChanges.getPreviousImportanceValue();
+
+        if (newImportanceValue === previousImportanceValue) {
+            newImportanceValue -= 1;
+        }
+
+        return newImportanceValue;
+    }
+
+    static getBoltContainerElement() {
+        let boltContainerElement;
+
+        if ($event.target.classList?.value?.includes('bolt-container')) {
+            boltContainerElement = $event.target;
+        } else {
+            boltContainerElement = $event.target.parentElement;
+        }
+
+        return boltContainerElement;
+    }
+
+    static getPreviousImportanceValue() {
+        return document.querySelectorAll('#create-edit-note-page-container .bolt.black').length;
+    }
+
+    static adjustElementColors(elements, classToAdd, classToRemove) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList?.add(classToAdd);
+            elements[i].classList?.remove(classToRemove);
+        }
+    }
+
+    static getCreateEditBoltsToBeElements(newImportanceValue) {
+        let createEditBoltsToBeBlackSelector;
+        let createEditBoltsToBeGraySelector;
+        const commaSpace = ', ';
+
+        for (let i = 1; i <= 5; i++) {
+            const createEditBoltSelector = `#create-edit-note-page-container .bolt-container[data-bolt-number="${i}"] .bolt`;
+
+            if (i <= newImportanceValue) {
+                if (i !== 1) {
+                    createEditBoltsToBeBlackSelector += commaSpace;
+                }
+                if (i === 1) {
+                    createEditBoltsToBeBlackSelector = createEditBoltSelector;
+                } else {
+                    createEditBoltsToBeBlackSelector += createEditBoltSelector;
+                }
+            } else {
+                if (i !== newImportanceValue + 1) {
+                    createEditBoltsToBeGraySelector += commaSpace;
+                }
+                if (i === newImportanceValue + 1) {
+                    createEditBoltsToBeGraySelector = createEditBoltSelector;
+                } else {
+                    createEditBoltsToBeGraySelector += createEditBoltSelector;
+                }
+            }
+        }
+
+        const createEditBoltsToBeBlackElements = createEditBoltsToBeBlackSelector
+            ? document.querySelectorAll(createEditBoltsToBeBlackSelector)
+            : [];
+        const createEditBoltsToBeGrayElements = createEditBoltsToBeGraySelector
+            ? document.querySelectorAll(createEditBoltsToBeGraySelector)
+            : [];
+
+        return {
+            createEditBoltsToBeBlackElements,
+            createEditBoltsToBeGrayElements,
+        };
+    }
+}
--- public/scripts/importance.component.mjs
+++ public/scripts/importance.component.mjs
@@ -1,3 +1,5 @@
+import ImportanceDomChanges from './importance-dom-changes.mjs';
+
 export default class ImportanceComponent {
     constructor(handlebars, isEditable = false) {
         this.handlebars = handlebars;
@@ -22,95 +24,23 @@ export default class ImportanceComponent {
         };
     }
 
-    adjustImportanceVisually($event) {
-        let boltContainerElement;
-        if ($event.target.classList?.value?.includes('bolt-container')) {
-            boltContainerElement = $event.target;
-        } else {
-            boltContainerElement = $event.target.parentElement;
-        }
-
-        let newImportanceValue = parseInt(
-            boltContainerElement.dataset?.boltNumber,
-        );
-
-        const previousImportanceValue = document.querySelectorAll(
-            '#create-edit-note-page-container .bolt.black',
-        ).length;
-
-        if (newImportanceValue === previousImportanceValue) {
-            newImportanceValue -= 1;
-        }
-
-        let createEditBoltsToBeBlackSelector;
-        let createEditBoltsToBeGraySelector;
-        const commaSpace = ', ';
-        for (let i = 1; i <= 5; i++) {
-            const createEditBoltSelector = `#create-edit-note-page-container .bolt-container[data-bolt-number="${i}"] .bolt`;
-
-            if (i <= newImportanceValue) {
-                if (i !== 1) {
-                    createEditBoltsToBeBlackSelector += commaSpace;
-                }
-                if (i === 1) {
-                    createEditBoltsToBeBlackSelector = createEditBoltSelector;
-                } else {
-                    createEditBoltsToBeBlackSelector += createEditBoltSelector;
-                }
-            } else {
-                if (i !== newImportanceValue + 1) {
-                    createEditBoltsToBeGraySelector += commaSpace;
-                }
-                if (i === newImportanceValue + 1) {
-                    createEditBoltsToBeGraySelector = createEditBoltSelector;
-                } else {
-                    createEditBoltsToBeGraySelector += createEditBoltSelector;
-                }
-            }
-        }
-        const createEditBoltsToBeBlackElements =
-            createEditBoltsToBeBlackSelector
-                ? document.querySelectorAll(createEditBoltsToBeBlackSelector)
-                : [];
-
-        const createEditBoltsToBeGrayElements = createEditBoltsToBeGraySelector
-            ? document.querySelectorAll(createEditBoltsToBeGraySelector)
-            : [];
-
-        for (let i = 0; i < createEditBoltsToBeBlackElements.length; i++) {
-            createEditBoltsToBeBlackElements[i].classList?.add('black');
-            createEditBoltsToBeBlackElements[i].classList?.remove('gray');
-        }
-
-        for (let i = 0; i < createEditBoltsToBeGrayElements.length; i++) {
-            createEditBoltsToBeGrayElements[i].classList?.add('gray');
-            createEditBoltsToBeGrayElements[i].classList?.remove('black');
-        }
-    }
-
     addImportanceElements(importanceList, importanceSelector, maxBolts = 5) {
-        const importanceElements =
-            document.querySelectorAll(importanceSelector);
+        const importanceElements = document.querySelectorAll(importanceSelector);
 
         for (let i = 0; i < importanceElements.length; i++) {
             const importance = importanceList[i];
             const importanceElement = importanceElements[i];
             for (let boltNumber = 1; boltNumber <= maxBolts; boltNumber++) {
                 // eslint-disable-next-line
-                const importanceContainerHtml =
-                    this.handlebars.templates.importance(
-                        this.getImportanceContext(importance, boltNumber),
-                    );
+                const importanceContainerHtml = this.handlebars.templates.importance(
+                    this.getImportanceContext(importance, boltNumber),
+                );
                 importanceElement.innerHTML += importanceContainerHtml;
-                for (let i = 0; i < importanceElement.children.length; i++) {
-                    if (
-                        importanceElement.children[
-                            i
-                        ].classList?.value?.includes('editable')
-                    ) {
-                        importanceElement.children[i].addEventListener(
+                for (let j = 0; j < importanceElement.children.length; j++) {
+                    if (importanceElement.children[j].classList?.value?.includes('editable')) {
+                        importanceElement.children[j].addEventListener(
                             'click',
-                            this.adjustImportanceVisually,
+                            ImportanceDomChanges.adjustImportanceColorsOnClick,
                         );
                     }
                 }
