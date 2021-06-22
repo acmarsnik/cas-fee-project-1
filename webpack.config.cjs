5a908b75f53871fee689e4323753fc4c8ea7dea9
albertmarsnik <albertmarsnik@gmail.com>
2021-06-22 22:34:20 +0200

    finish up basics of dark-mode and rainbow styles

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,9 +3,17 @@ module.exports = {
     entry: [
         './public/scripts/startup.mjs',
         './public/styles/index.css',
+        './public/styles/index-dark-mode.css',
+        './public/styles/index-rainbow.css',
         './public/styles/importance.css',
+        './public/styles/importance-dark-mode.css',
+        './public/styles/importance-rainbow.css',
         './public/styles/common.css',
+        './public/styles/common-dark-mode.css',
+        './public/styles/common-rainbow.css',
         './public/styles/create-edit-note.css',
+        './public/styles/create-edit-note-dark-mode.css',
+        './public/styles/create-edit-note-rainbow.css',
     ],
     output: {
         path: __dirname,
